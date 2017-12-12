43651f09020aa4df7fae0ba0d968ae9cc2a01a89
Fir3AtWill <Fir3AtWill@DESKTOP-94C6DB6>
2017-12-11 20:41:21 -0500

    First Commit, Completion of all requirements, added bonus is that twitter was added for part 1

---
+++ WebApplication2/Controllers/PLocationsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class PLocationsController : Controller
+    {
+        private Comp2007Sept28_Ass2Entities db = new Comp2007Sept28_Ass2Entities();
+
+        // GET: PLocations
+        public ActionResult Index()
+        {
+            return View(db.PLocations.ToList());
+        }
+
+        // GET: PLocations/Details/5
+        [Authorize]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PLocation pLocation = db.PLocations.Find(id);
+            if (pLocation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pLocation);
+        }
+
+        // GET: PLocations/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PLocations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "LocationID,City,Country,ProvinceOrState")] PLocation pLocation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PLocations.Add(pLocation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pLocation);
+        }
+
+        // GET: PLocations/Edit/5
+        [Authorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PLocation pLocation = db.PLocations.Find(id);
+            if (pLocation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pLocation);
+        }
+
+        // POST: PLocations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "LocationID,City,Country,ProvinceOrState")] PLocation pLocation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pLocation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pLocation);
+        }
+
+        // GET: PLocations/Delete/5
+        [Authorize]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PLocation pLocation = db.PLocations.Find(id);
+            if (pLocation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pLocation);
+        }
+
+        // POST: PLocations/Delete/5
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PLocation pLocation = db.PLocations.Find(id);
+            db.PLocations.Remove(pLocation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication2/Controllers/PartiesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class PartiesController : Controller
+    {
+        private Comp2007Sept28_Ass2Entities db = new Comp2007Sept28_Ass2Entities();
+
+        // GET: Parties
+        public ActionResult Index()
+        {
+            var parties = db.Parties.Include(p => p.PLocation);
+            return View(parties.ToList());
+        }
+
+        // GET: Parties/Details/5
+        [Authorize]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Party party = db.Parties.Find(id);
+            if (party == null)
+            {
+                return HttpNotFound();
+            }
+            return View(party);
+        }
+
+        // GET: Parties/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            ViewBag.LocationID = new SelectList(db.PLocations, "LocationID", "City");
+            return View();
+        }
+
+        // POST: Parties/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PartyId,Message_,Party_Type_,Address_,LocationID")] Party party)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Parties.Add(party);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.LocationID = new SelectList(db.PLocations, "LocationID", "City", party.LocationID);
+            return View(party);
+        }
+
+        // GET: Parties/Edit/5
+        [Authorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Party party = db.Parties.Find(id);
+            if (party == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.LocationID = new SelectList(db.PLocations, "LocationID", "City", party.LocationID);
+            return View(party);
+        }
+
+        // POST: Parties/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PartyId,Message_,Party_Type_,Address_,LocationID")] Party party)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(party).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.LocationID = new SelectList(db.PLocations, "LocationID", "City", party.LocationID);
+            return View(party);
+        }
+
+        // GET: Parties/Delete/5
+        [Authorize]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Party party = db.Parties.Find(id);
+            if (party == null)
+            {
+                return HttpNotFound();
+            }
+            return View(party);
+        }
+
+        // POST: Parties/Delete/5
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Party party = db.Parties.Find(id);
+            db.Parties.Remove(party);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
